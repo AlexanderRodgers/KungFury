5268a5a5dfae38cdec310a510f38b0fc2ec5fce0
Alex Rodgers <alex.e.roders@gmail.com>
2017-02-20 19:06:30 -0800

    Stopped somewhere I don't remembember, probably won't work

--- FuryTest.java
+++ FuryTest.java
@@ -2,29 +2,34 @@ import java.util.*;
 
 public class FuryTest {
 
-    static Place[][] arena1 = new Place[2][2];
+    static Place[][] arena1 = new Place[3][3];
     static ArrayList<Item> furyItems = new ArrayList<Item>();
     static Hero p1 = new Hero("Kung fury", "The baddest boy", 20.0, 150.0);
     static String move = "";
     static Scanner scan = new Scanner(System.in);
     static int checkLength = 3;
 
-    static String help = "Commands:\n-North\n-South\n-East\n-West\n-Inv\n-Look\n-Fight-HP\n-Moves-Info";
+    static String help = "Commands:\n-North\n-South\n-East\n-West\n-Inv\n-Look\n-Fight\n-Health\n-Info";
 
     public static void main(String[] args) {
 
         //Place[][] arena2 = new Place[2][2];
 
-        arena1[0][0] = new Place("home", "skyscraper");
-        arena1[0][1] = new Place("Place1","hell");
-        arena1[1][0] = new Place("Place2", "heaven");
-        arena1[1][1] = new Place("place3", "shit");
-
+        arena1[0][0] = new Place("the home of Hackerman", "Pardon the cheeto dust.");
+        arena1[0][1] = new Place("Hackerman's neighboorhood","Hackerman must be nearby.");
+        arena1[0][2] = new Place("Hackerman's neighbor", "Say you're sorry for barging in.");
+        arena1[1][0] = new Place("Oak Ridge High School", "Softly, in the distance you hear the cry of children.");
+        arena1[1][1] = new Place("the Sheriffs office", "The Führer destroyed the place.");
+        //Need to add a heal command
+        arena1[1][2] = new Place("the hospital", "Heal up");
+        arena1[2][0] = new Place("he home of the president", "Trump");
+        arena1[2][1] = new Place("your house", "A lovely little place");
+        arena1[2][2] = new Place("the arcade", "It's full of stuff.");
+        
         System.out.print("\f");
-        boolean playAgain = true;
-
-        p1.setCoords(0, 0);
-
+        boolean isAlive = true;
+        p1.setCoords(2, 1);
+        
         //Must be a square
         p1.updateMax(arena1.length, arena1[0].length);
 
@@ -39,21 +44,22 @@ public class FuryTest {
             "To do this you must find hackerman so he can hack you back in time\nYou'll" +
             " know what to do when you get there.\n\nYou can type \"help\" at any time to see a list of moves.");
 
-        while(playAgain) {
-
+        while(isAlive) {
+            
             System.out.println(generateMap());
             System.out.println();
 
+            System.out.println("You are in " + arena1[p1.getX()][p1.getY()].toString() + "\n");
             System.out.println("Where do you want to go?");
             move = scan.next();
             filterAll(move);
-            System.out.println(generateMap());
+            if(hasEnemy()) {
+                //Stopped here? I don't remember.
+                System.out.println("You cannot move there are enemies nearby.");
+            }
 
-            move = scan.next();
-            filterAll(move);
-            System.out.println(generateMap());
-
-            playAgain = false;
+            if(p1.checkIfDead())
+                isAlive = false;
         }
     }
 
@@ -126,6 +132,12 @@ public class FuryTest {
                 //Stopped here
                 printPeople1();
             }
+            else if(input.substring(0, 3).equalsIgnoreCase("hea")) {
+                System.out.println("You have: " + p1.getHealth() + " health.");
+            }
+            else if(input.substring(0, 3).equalsIgnoreCase("inf")) {
+                System.out.println(getInfo());
+            }
             else {
                 System.out.println("What do you want to do?");
                 move = scan.next();
@@ -162,10 +174,30 @@ public class FuryTest {
 
             String peopleInRoom = "People here: ";
             for(int i = 0; i < inRoom.size(); i++) {
+                //Check to see if instance of enemy this needs to be revised.
                 peopleInRoom += inRoom.get(i).toString();
             }
-            
+
         }
+    }
+
+    public boolean hasEnemy() {
+        ArrayList<Person> enemies = arena1[p1.getX()][p1.getY()].getPeople();
+        for(int i = 0; i < enemies.size(); i++) {
+            if(enemies.get(i) instanceof Enemy) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static String getInfo() {
+        String totalInfo = "";
+        //This breaks every other time for because p1 doesn't get initialized first?
+        totalInfo += "You are in " + arena1[p1.getX()][p1.getY()].toString() + "\n";
+        totalInfo += "You have " + p1.getHealth() + " health\n";
+        totalInfo += "You have " + furyItems.size() + " items in your inventory.\n";
+        return totalInfo;
     }
 
 }
